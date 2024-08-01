a2db5c40e6381f9c0e9ff4e2a02087b9157631c2
Chetand777 <chetandaddikar31@gmail.com>
2024-08-01 21:45:59 +0530

    setup and requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+
+  name="us_visa",
+  version="0.0.0",
+  author="Chetand777",
+  author_email="chetandaddikar31@gmail.com",
+  packages=find_packages()
+
+)
